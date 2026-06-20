8ea32355e6a7d2dcb766b0f3b384961c69e53f27
Muhammad Ahsan Nasir <ahsangamedev66@gmail.com>
2026-06-21 01:36:59 +0500

    Add files via upload

---
+++ Flappy Bird/Assets/SCRIPTS/Parallax.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+    public float animationSpeed = 1;
+
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        meshRenderer.material.mainTextureOffset += new Vector2(animationSpeed * Time.deltaTime, 0);
+    }
+}
---
+++ Flappy Bird/Assets/SCRIPTS/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public float spawnRate = 1f; // the amount of seconds
+    public float minHeight = -1f;
+    public float maxHeight = 1f;
+
+
+    private void OnEnable()
+    {
+        InvokeRepeating(nameof(Spawn), spawnRate, spawnRate);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke(nameof(Spawn));
+    }
+
+
+
+    private void Spawn()
+    {
+        GameObject pipes = Instantiate(prefab, transform.position, Quaternion.identity);
+        pipes.transform.position += Vector3.up * Random.Range(minHeight, maxHeight);
+
+    }
+
+
+}
